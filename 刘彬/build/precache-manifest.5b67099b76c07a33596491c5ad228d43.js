4cc4dbeb7a5ca4758e0dc5d81e286f01b5c37834
qianxunlingbao <1433917529@qq.com>
2019-12-20 15:03:51 +0800

    add.

---
+++ 刘彬/build/precache-manifest.5b67099b76c07a33596491c5ad228d43.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1a668bfba20c94de8b0ca86f11be4c2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4e9c42335a06ef5de2cb",
+    "url": "/static/css/main.8b010d6b.chunk.css"
+  },
+  {
+    "revision": "b1c9a1e2d0aa1fb3f4e3",
+    "url": "/static/js/2.0193af58.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.0193af58.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4e9c42335a06ef5de2cb",
+    "url": "/static/js/main.6ce3c6d4.chunk.js"
+  },
+  {
+    "revision": "3b538de4c9a31f22aba2",
+    "url": "/static/js/runtime-main.13d51fa7.js"
+  }
+]);
